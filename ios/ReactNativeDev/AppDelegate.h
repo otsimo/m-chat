6033a89dd535c45a9d19c360b96283cc6bcbe26b
Batuhan Ünver <batuhan@otsimo.com>
2017-02-23 13:13:29 +0300

    Merge android and iOS versions

---
+++ ios/ReactNativeDev/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReactNativeDev
+//
+//  Created by Batuhan on 23/02/2017.
+//  Copyright © 2017 mdemirci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
