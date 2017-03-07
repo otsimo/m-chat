db09d071e9ffd04ab3fa7c8ded3d37475082a618
muhammetdemirci <mddemircii5@gmail.com>
2017-03-07 20:17:46 +0300

    FormStep extension

---
+++ Otsimo Test/FormProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  FormProtocol.swift
+//  OtsimoTest
+//
+//  Created by demirci on 07/03/2017.
+//  Copyright © 2017 mdemirci. All rights reserved.
+//
+
+import Foundation
---
+++ Otsimo Test/ORKFormStep+Result.swift
@@ -0,0 +1,9 @@
+//
+//  ORKFormStep+Result.swift
+//  OtsimoTest
+//
+//  Created by demirci on 07/03/2017.
+//  Copyright © 2017 mdemirci. All rights reserved.
+//
+
+import Foundation
